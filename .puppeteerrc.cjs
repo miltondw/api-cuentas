d085f24e6620faf053ba7426f0bac464019d15f0
miltondw <estradamilton2001@gmail.com>
2025-05-24 19:15:08 -0500

    feat: Add Puppeteer configuration file and update package.json for browser installation

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,10 @@
+// .puppeteerrc.cjs
+const { join } = require("path");
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Cambia la ubicación del caché para Puppeteer
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+};
